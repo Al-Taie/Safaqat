af9afcab0f2a71b970e4606b65ecd4bff399e473
Al-Taie <Ahmed.m@the-gw.com>
2022-08-27 17:06:55 +0300

    create forgot screen

---
+++ lib/safaqat/presentation/ui/auth/forgot/forgot_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:safaqat/safaqat/presentation/ui/auth/forgot/forgot_controller.dart';
+
+class ForgotPage extends StatelessWidget {
+  const ForgotPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(ForgotController());
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
