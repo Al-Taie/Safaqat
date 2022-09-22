577b8c670f37397c100778089695110264896ce5
Al-Taie <Ahmed.m@the-gw.com>
2022-09-22 19:13:15 +0300

    create controllers

--- lib/safaqat/presentation/ui/auth/login/login_controller.dart
+++ lib/safaqat/presentation/ui/auth/login/login_controller.dart
@@ -1,14 +1,41 @@
 import 'package:get/get.dart';
+import 'package:safaqat/safaqat/data/models/auth/login/login_body.dart';
+import 'package:safaqat/safaqat/data/models/auth/login/login_dto.dart';
+import 'package:safaqat/safaqat/domain/entities/resources.dart';
+import 'package:safaqat/safaqat/domain/usecase/auth/login/login_usecase.dart';
+import 'package:safaqat/safaqat/domain/usecase/auth/login/save_use_info_usecase.dart';
 
 class LoginController extends GetxController {
-  LoginController() {}
+  final LoginUseCase _loginUseCase = Get.find();
+  final SaveUserInfoUseCase _saveUserInfoUseCase = Get.find();
 
-  final _email = ''.obs;
-  String get email => _email.value;
-  set email(String value) => _email.value = value;
+  final _username = ''.obs;
+  String get username => _username.value;
+  set username(String value) => _username.value = value;
 
   final _password = ''.obs;
   String get password => _password.value;
   set password(String value) => _password.value = value;
 
+  Rx<Resources<LoginDto>> auth =
+      Resources<LoginDto>.init().obs;
+
+  void login() async {
+    final body = LoginBody(
+      username: username,
+      password: password
+    );
+
+    auth.value = Resources.loading();
+
+    final result = await _loginUseCase(params: body);
+    _saveUserInfo(result.data);
+    auth.value = result;
+  }
+
+  void _saveUserInfo(LoginDto? info) {
+    if (info != null) {
+      _saveUserInfoUseCase(params: info);
+    }
+  }
 }
