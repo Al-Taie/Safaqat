a7fe3c3b07184158831ea216908f6c54a16abe73
Al-Taie <Ahmed.m@the-gw.com>
2022-12-24 13:26:33 +0300

    fix seach on map

--- lib/safaqat/presentation/ui/events/map/search_location_field.dart
+++ lib/safaqat/presentation/ui/events/map/search_location_field.dart
@@ -29,72 +29,80 @@ class LocationSearchField extends StatelessWidget {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         child: SizedBox(
           width: Get.width,
-          child: TypeAheadField(
-            textFieldConfiguration: TextFieldConfiguration(
-              controller: textController,
-              textInputAction: TextInputAction.search,
-              autofocus: true,
-              onSubmitted: (query) {
-                var result = controller.findInSearchResults(query);
-                if (result != null) {
-                  onPlaceSelected?.call(result);
-                  Get.back();
-                }
-              },
-              textCapitalization: TextCapitalization.words,
-              keyboardType: TextInputType.streetAddress,
-              decoration: InputDecoration(
-                hintText: AppStrings.search,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide:
-                      const BorderSide(style: BorderStyle.none, width: 0),
-                ),
-                hintStyle: Theme.of(context).textTheme.headline2?.copyWith(
-                      fontSize: 16,
-                      color: Theme.of(context).disabledColor,
-                    ),
-                filled: true,
-                fillColor: Theme.of(context).cardColor,
-              ),
-              style: Theme.of(context).textTheme.headline2?.copyWith(
-                    color: Theme.of(context).textTheme.bodyText1?.color,
-                    fontSize: 16,
+          child: SingleChildScrollView(
+            child: TypeAheadField(
+              textFieldConfiguration: TextFieldConfiguration(
+                controller: textController,
+                textInputAction: TextInputAction.search,
+                autofocus: true,
+                onSubmitted: (query) async {
+                  var result = controller.findInSearchResults(query);
+                  if (result != null) {
+                    onPlaceSelected?.call(result);
+                    Get.back();
+                  }
+                },
+                textCapitalization: TextCapitalization.words,
+                keyboardType: TextInputType.streetAddress,
+                decoration: InputDecoration(
+                  hintText: AppStrings.search,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide:
+                        const BorderSide(style: BorderStyle.none, width: 0),
                   ),
-            ),
-            suggestionsCallback: (query) async {
-              controller.placeSearch(query: query);
-              return controller.searchResults;
-            },
-            itemBuilder: (context, PlacesSearchResult suggestion) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: Row(
-                  children: [
-                    const Icon(Icons.location_on,
-                        color: AppColors.primaryColor),
-                    const SizedBox(width: 5),
-                    Expanded(
-                      child: Text(
-                        suggestion.name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.headline2?.copyWith(
-                              color:
-                                  Theme.of(context).textTheme.bodyText1?.color,
-                              fontSize: 16,
-                            ),
-                      ),
-                    ),
-                  ],
+                  hintStyle:
+                      Theme.of(context).textTheme.displayMedium?.copyWith(
+                            fontSize: 16,
+                            color: Theme.of(context).disabledColor,
+                          ),
+                  filled: true,
+                  fillColor: Theme.of(context).cardColor,
                 ),
-              );
-            },
-            onSuggestionSelected: (PlacesSearchResult suggestion) {
-              onPlaceSelected?.call(suggestion);
-              Get.back();
-            },
+                style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                      color: Theme.of(context).textTheme.bodyLarge?.color,
+                      fontSize: 16,
+                    ),
+              ),
+              suggestionsCallback: (query) async {
+                controller.placeSearch(query: query);
+                return controller.searchResults;
+              },
+              itemBuilder: (context, PlacesSearchResult suggestion) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Row(
+                    children: [
+                      const Icon(Icons.location_on,
+                          color: AppColors.primaryColor),
+                      const SizedBox(width: 5),
+                      Expanded(
+                        child: Text(
+                          suggestion.name,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayMedium
+                              ?.copyWith(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge
+                                    ?.color,
+                                fontSize: 16,
+                              ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              onSuggestionSelected: (PlacesSearchResult suggestion) async {
+                onPlaceSelected?.call(suggestion);
+                Get.back();
+              },
+            ),
           ),
         ),
       ),
