7fa74f7a9c79f1e69b9564675b1dae4e83c695f2
Al-Taie <Ahmed.m@the-gw.com>
2023-01-18 18:40:06 +0300

    navigate to manage contracts

--- lib/safaqat/presentation/ui/events/public/events_page.dart
+++ lib/safaqat/presentation/ui/events/public/events_page.dart
@@ -7,6 +7,7 @@ import 'package:safaqat/safaqat/app/extensions/list_extension.dart';
 import 'package:safaqat/safaqat/app/utils/utils.dart';
 import 'package:safaqat/safaqat/presentation/custom_views/app_bar_widget.dart';
 import 'package:safaqat/safaqat/presentation/custom_views/status_widget.dart';
+import 'package:safaqat/safaqat/presentation/ui/contracts/mine/my_contracts_page.dart';
 import 'package:safaqat/safaqat/presentation/ui/events/components/event_card_widget.dart';
 import 'package:safaqat/safaqat/presentation/ui/events/details/event_details_page.dart';
 import 'package:safaqat/safaqat/presentation/ui/events/mine/my_events_page.dart';
@@ -41,6 +42,7 @@ class EventsPage extends StatelessWidget {
                   onManageEvents: const MyEventsPage().navTo,
                   onManagePosts: const MyPostsPage().navTo,
                   onManageProjects: const MyProjectsPage().navTo,
+                  onManageContracts: const MyContractsPage().navTo,
                   onLogout: controller.logout,
                 )
               : null,
--- lib/safaqat/presentation/ui/posts/public/posts_page.dart
+++ lib/safaqat/presentation/ui/posts/public/posts_page.dart
@@ -5,6 +5,7 @@ import 'package:safaqat/safaqat/app/extensions/list_extension.dart';
 import 'package:safaqat/safaqat/app/utils/utils.dart';
 import 'package:safaqat/safaqat/presentation/custom_views/app_bar_widget.dart';
 import 'package:safaqat/safaqat/presentation/custom_views/status_widget.dart';
+import 'package:safaqat/safaqat/presentation/ui/contracts/mine/my_contracts_page.dart';
 import 'package:safaqat/safaqat/presentation/ui/events/mine/my_events_page.dart';
 import 'package:safaqat/safaqat/presentation/ui/news/components/app_drawer.dart';
 import 'package:safaqat/safaqat/presentation/ui/news/mine/my_news_page.dart';
@@ -38,6 +39,7 @@ class PostsPage extends StatelessWidget {
                   onManageEvents: const MyEventsPage().navTo,
                   onManagePosts: const MyPostsPage().navTo,
                   onManageProjects: const MyProjectsPage().navTo,
+                  onManageContracts: const MyContractsPage().navTo,
                   onLogout: controller.logout,
                 )
               : null,
